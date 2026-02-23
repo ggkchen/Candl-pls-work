ed13b7ff57056e85b3a26ed0cdc259c32a6879cd
jamespotato <12345gavinchen@gmail.com>
2026-02-22 20:58:15 -0800

    Initial commit

--- src/main/java/frc/robot/subsystems/CANdleSubsystem.java
+++ src/main/java/frc/robot/subsystems/CANdleSubsystem.java
@@ -1,30 +1,33 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.led.CANdle;
-import com.ctre.phoenix.led.CANdleConfiguration;
-import com.ctre.phoenix.led.RainbowAnimation;
+import com.ctre.phoenix6.hardware.CANdle;
+import com.ctre.phoenix6.configs.CANdleConfiguration;
+import com.ctre.phoenix6.controls.SolidColor;
+import com.ctre.phoenix6.controls.RainbowAnimation;
+import com.ctre.phoenix6.signals.RGBWColor;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class CANdleSubsystem extends SubsystemBase {
 
+    private static final int LED_START = 0;
+    private static final int LED_END = 59;
+
     private final CANdle candle = new CANdle(10);
 
     public CANdleSubsystem() {
         CANdleConfiguration config = new CANdleConfiguration();
-        config.brightnessScalar = 0.5;
-
-        candle.configAllSettings(config);
-        candle.setLEDs(0, 0, 0);
+        config.LED.BrightnessScalar = 0.5;
+        candle.getConfigurator().apply(config);
     }
 
     public void setColor(int r, int g, int b) {
-        candle.clearAnimation(0);
-        candle.setLEDs(r, g, b);
+        candle.setControl(new SolidColor(LED_START, LED_END)
+                .withColor(new RGBWColor(r, g, b)));
     }
 
     public void setRainbow() {
-        RainbowAnimation rainbow = new RainbowAnimation(1.0, 0.5, 60);
-        candle.animate(rainbow);
+        candle.setControl(new RainbowAnimation(LED_START, LED_END)
+                .withBrightness(1.0));
     }
 }
